c62e4a905893e1fff12c6e2378627f26faa96cc5
saniok6789 <english.polyak@gmail.com>
2023-09-24 10:34:04 +0300

    add files

--- iss_tracker.py
+++ iss_tracker.py
@@ -2,27 +2,27 @@ import requests
 import csv
 import time
 
-# URL для отримання даних про положення МКС
+
 iss_url = "http://api.open-notify.org/iss-now.json"
 
 
-# Функція для отримання даних про положення МКС
+
 def get_iss_position():
-    response = requests.get(iss_url)
-    data = response.json()
+    response=requests.get(iss_url)
+    data=response.json()
     return data['iss_position']
 
 
-# Назва CSV-файлу для зберігання даних
-csv_file = 'iss_position.csv'
+
+csv_file='iss_position.csv'
 
 while True:
-    # Отримуємо дані про положення МКС
-    iss_position = get_iss_position()
 
-    # Записуємо дані у CSV-файл
+    iss_position=get_iss_position()
+
+
     with open(csv_file, mode='a', newline='') as file:
-        writer = csv.writer(file, delimiter=';')
+        writer=csv.writer(file, delimiter=';')
         writer.writerow([iss_position['latitude'], iss_position['longitude']])
 
     print(f"Записано дані: Широта={iss_position['latitude']}, Довгота={iss_position['longitude']}")
